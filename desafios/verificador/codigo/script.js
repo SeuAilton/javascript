041743bff83216001d25ea57b613155504375e19
SeuAilton <123329775+SeuAilton@users.noreply.github.com>
2024-01-25 21:29:04 -0300

    desafio verificador
    consegui fazer as imagens serem geradas aleatoriamente

--- desafios/verificador/codigo/script.js
+++ desafios/verificador/codigo/script.js
@@ -18,27 +18,27 @@ function verificar() {
     var imagemAleatoria = Math.floor(Math.random() * 6)
 
     if (sexo = "Masculino" && idade > 0 && idade < 6){
-
-    }else if (idade > 5){
-
-    }else if (idade > 12){
-
-    }else if (idade > 17){
-
+        document.getElementById("imagem-img").src = `imagens/bebes/masculino/${imagemAleatoria}.png`
+    }else if (idade > 5 && idade < 13){
+        document.getElementById("imagem-img").src = `imagens/criancas/masculino/${imagemAleatoria}.png`
+    }else if (idade > 12 && idade < 18){
+        document.getElementById("imagem-img").src = `imagens/jovens/masculino/${imagemAleatoria}.png`
+    }else if (idade > 17 && idade < 40){
+        document.getElementById("imagem-img").src = `imagens/adultos/masculino/${imagemAleatoria}.png`
     }else if (idade > 39){
-
+        document.getElementById("imagem-img").src = `imagens/velhos/masculino/${imagemAleatoria}.png`
     }
 
     if (sexo = "Feminino" && idade > 0 && idade < 6){
-
-    }else if (idade > 5){
-
-    }else if (idade > 12){
-
-    }else if (idade > 17){
-
+        document.getElementById("imagem-img").src = `imagens/bebes/feminino/${imagemAleatoria}.png`
+    }else if (idade > 5 && idade < 13){
+        document.getElementById("imagem-img").src = `imagens/criancas/feminino/${imagemAleatoria}.png`
+    }else if (idade > 12 && idade < 18){
+        document.getElementById("imagem-img").src = `imagens/jovens/feminino/${imagemAleatoria}.png`
+    }else if (idade > 17 && idade < 40){
+        document.getElementById("imagem-img").src = `imagens/adultos/feminino/${imagemAleatoria}.png`
     }else if (idade > 39){
-
+        document.getElementById("imagem-img").src = `imagens/velhos/feminino/${imagemAleatoria}.png`
     }
 
     document.getElementById("resultado").innerText = `Sua idade é ${idade} anos`
