f09e0ae2d4053e2ab66f8ed3bce5a2a85fcf04e8
SeuAilton <123329775+SeuAilton@users.noreply.github.com>
2024-01-26 18:17:14 -0300

    desafio verificador
    quase pronto falta somente pequenos ajustes

--- desafios/verificador/codigo/script.js
+++ desafios/verificador/codigo/script.js
@@ -14,34 +14,43 @@ function verificar() {
         idade--;
     }
 
-    var sexo = document.querySelector('input[name="sexo"]:checked').value 
+    var amor = document.getElementsByName("sexo")
+    var genero = ''
     var imagemAleatoria = Math.floor(Math.random() * 6)
-
-    if (sexo = "Masculino" && idade > 0 && idade < 6){
-        document.getElementById("imagem-img").src = `imagens/bebes/masculino/${imagemAleatoria}.png`
-    }else if (idade > 5 && idade < 13){
-        document.getElementById("imagem-img").src = `imagens/criancas/masculino/${imagemAleatoria}.png`
-    }else if (idade > 12 && idade < 18){
-        document.getElementById("imagem-img").src = `imagens/jovens/masculino/${imagemAleatoria}.png`
-    }else if (idade > 17 && idade < 40){
-        document.getElementById("imagem-img").src = `imagens/adultos/masculino/${imagemAleatoria}.png`
-    }else if (idade > 39){
-        document.getElementById("imagem-img").src = `imagens/velhos/masculino/${imagemAleatoria}.png`
-    }
-
-    if (sexo = "Feminino" && idade > 0 && idade < 6){
-        document.getElementById("imagem-img").src = `imagens/bebes/feminino/${imagemAleatoria}.png`
-    }else if (idade > 5 && idade < 13){
-        document.getElementById("imagem-img").src = `imagens/criancas/feminino/${imagemAleatoria}.png`
-    }else if (idade > 12 && idade < 18){
-        document.getElementById("imagem-img").src = `imagens/jovens/feminino/${imagemAleatoria}.png`
-    }else if (idade > 17 && idade < 40){
-        document.getElementById("imagem-img").src = `imagens/adultos/feminino/${imagemAleatoria}.png`
-    }else if (idade > 39){
-        document.getElementById("imagem-img").src = `imagens/velhos/feminino/${imagemAleatoria}.png`
-    }
-
     document.getElementById("resultado").innerText = `Sua idade é ${idade} anos`
-    
+
+    if (amor[0].checked){
+        genero = 'Masculino'
+        if (idade <= 0) {
+            document.getElementById("imagem-img").src = "imagens/coco/mr-hankey.png"
+            document.getElementById("resultado").innerText = "Voçê ainda não nasceu!"
+        }else if (idade < 4){
+                document.getElementById("imagem-img").src = `imagens/bebes/masculino/${imagemAleatoria}.png`
+        }else if (idade < 12){
+                document.getElementById("imagem-img").src = `imagens/criancas/masculino/${imagemAleatoria}.png`
+        }else if (idade < 18){
+                document.getElementById("imagem-img").src = `imagens/jovens/masculino/${imagemAleatoria}.png`
+        }else if (idade < 50){
+                document.getElementById("imagem-img").src = `imagens/adultos/masculino/${imagemAleatoria}.png`
+        }else {
+                document.getElementById("imagem-img").src = `imagens/velhos/masculino/${imagemAleatoria}.png`
+        }   
+    }
+    else if (amor[1].checked){
+        genero = 'Feminino'
+        if (idade <= 0){
+            document.getElementById("imagem-img").src = "imagens/coco/mr-hankey.png"
+            document.getElementById("resultado").innerText = "Voçê ainda não nasceu!"
+        }else if (idade < 4){
+                document.getElementById("imagem-img").src = `imagens/bebes/feminino/${imagemAleatoria}.png`
+        }else if (idade < 12){
+                document.getElementById("imagem-img").src = `imagens/criancas/feminino/${imagemAleatoria}.png`
+        }else if (idade < 18){
+                document.getElementById("imagem-img").src = `imagens/jovens/feminino/${imagemAleatoria}.png`
+            }else if (idade < 50){
+                document.getElementById("imagem-img").src = `imagens/adultos/feminino/${imagemAleatoria}.png`
+        }else {
+                document.getElementById("imagem-img").src = `imagens/velhos/feminino/${imagemAleatoria}.png`
+        } 
+    }  
 }
-    document.getElementById("data").addEventListener("click", verificar)
