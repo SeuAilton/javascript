aec862b496543faacfb7460d79c991f1473e116b
SeuAilton <123329775+SeuAilton@users.noreply.github.com>
2024-02-01 17:20:58 -0300

    Repetições
    while, do while, for

---
+++ aula07/ambiente.js
@@ -0,0 +1,13 @@
+var contador = 1
+do {
+    console.log(`Conoite ${contador}`)
+    contador++
+}while (contador <= 6)
+
+/*
+var contador = 1
+while (contador <= 10) {
+    console.log(`Condia ${contador}`)
+    contador++
+}
+*/
---
+++ aula08/ambiente.js
@@ -0,0 +1,11 @@
+/*
+var c = 1
+while (c <= 5) {
+    console.log(`teste ${c}`)
+    c++
+}
+*/
+
+for(var c = 1;c <= 5;c++) {
+    console.log(`teste ${c}`)
+}
